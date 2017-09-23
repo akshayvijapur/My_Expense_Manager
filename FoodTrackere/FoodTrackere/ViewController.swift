037c88370d93c2f81e6fc525724263ffa23c0331
STUDY-NOW <study-now@STUDY-NOWs-iMac.local>
2017-09-23 14:54:57 +0530

    akshay

---
+++ FoodTrackere/FoodTrackere/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FoodTrackere
+//
+//  Created by STUDY-NOW on 23/09/17.
+//  Copyright © 2017 STUDY-NOW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+ 
+
+}
+
